7524c22c0483d7c58ac4320207ac1682cfbe7d3b
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-03 14:39:07 +0000

    liste mit vorgefertigten notizen

--- app/src/main/java/com/example/notes/data/Repository.kt
+++ app/src/main/java/com/example/notes/data/Repository.kt
@@ -1,4 +1,16 @@
 package com.example.notes.data
 
+import com.example.notes.data.model.Note
+import java.util.*
+
 class Repository {
+
+    fun loadNotes(): MutableList<Note> {
+        return mutableListOf<Note>(
+            Note("Einkaufsliste", "Tomaten, Ingwer, Orangen, Zitronen, Reis, Kartoffeln", Date(2022,12,1), Date(2022,12,2)),
+            Note("To Do", "Lernen, Putzen, Kochen, Sport, Gassi gehen, Hund füttern", Date(2022,12,1), Date(2022,12,2)),
+            Note("Boy Baby Names", "Angelo, Vincent, Timothy, Simon, David, Max, Joaquin", Date(2022,12,1), Date(2022,12,2)),
+            Note("Girl Baby Names", "Carla, Gina, Gigi, Gia", Date(2022,12,1), Date(2022,12,2)),
+        )
+    }
 }
