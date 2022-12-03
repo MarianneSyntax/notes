e191787690d4bc83e43940512596f55669dbe1b9
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-03 14:05:21 +0000

    NoteAdapter.kt skelett angelegt: der Adapter kümmert sich darum, dass in die list items in der RecyclerView die korrekten Daten reingeladen werden.

---
+++ app/src/main/java/com/example/notes/NoteAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.notes
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.ItemViewHolder>() {
+
+    inner class ItemViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
