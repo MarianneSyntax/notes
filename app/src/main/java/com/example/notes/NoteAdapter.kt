a8442c20bc297e1ec3d562c3fd5c1b65e956d6bb
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-03 14:50:28 +0000

    ADAPTER - 3. SCHRITT: onBindViewHolder
    1. dem Adapter einen datensatz dataset geben, der die Liste an Notizen aus dem Repository speichert
    
    2. Binde die Daten an der Position position aus dem Repository an die Elemente in der notiz item View
    
    3. setze schonmal den setOnClickListener, der zum DetailScreen der angeklickten notiz items führen soll
    
    ADAPTER soweit FERTIG.

--- app/src/main/java/com/example/notes/NoteAdapter.kt
+++ app/src/main/java/com/example/notes/NoteAdapter.kt
@@ -3,16 +3,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.notes.data.Repository
 
 class NoteAdapter: RecyclerView.Adapter<NoteAdapter.ItemViewHolder>() {
+    val dataset = Repository().loadNotes()
 
     inner class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val title: TextView = itemView.findViewById(R.id.note_title)
         val body: TextView = itemView.findViewById(R.id.note_body)
         val dateCreated: TextView = itemView.findViewById(R.id.note_created)
         val dateEdited: TextView = itemView.findViewById(R.id.note_edited)
-        val noteCard: TextView = itemView.findViewById(R.id.note_card)
+        val noteCard: CardView = itemView.findViewById(R.id.note_card)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -23,8 +26,17 @@ class NoteAdapter: RecyclerView.Adapter<NoteAdapter.ItemViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.title.text = dataset[position].title
+        holder.body.text = dataset[position].body
+        holder.dateCreated.text = dataset[position].dateCreated.toString()
+        holder.dateEdited.text = dataset[position].dateEdited.toString()
+
+        holder.noteCard.setOnClickListener {
+            //TODO: implementieren
+        }
+
     }
+
 
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
