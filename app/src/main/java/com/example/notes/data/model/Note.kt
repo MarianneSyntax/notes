55fd6bf45b0d19c791cacb4693b731547198ad33
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-03 14:31:45 +0000

    notiz model mit den Eigenschaften, die eine Notiz hat

--- app/src/main/java/com/example/notes/data/model/Note.kt
+++ app/src/main/java/com/example/notes/data/model/Note.kt
@@ -1,4 +1,7 @@
 package com.example.notes.data.model
 
-class Note {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.*
+@Parcelize
+data class Note(val title: String, val body: String, val dateCreated: Date, val dateEdited: Date) : Parcelable
