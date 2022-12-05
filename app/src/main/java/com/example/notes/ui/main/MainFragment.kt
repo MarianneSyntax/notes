de797c0356d4dc8cbb833d31c25560f05d484b11
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-05 10:36:47 +0000

    MainFragment: Schritt 3 - onViewCreated
    Nachdem die View unseres MainFragments erstellt ist, wird diese Funktion aufgerufen.
    Hier können wir unserem MainFragment mitteilen, dass die Recyclerview "noteRecycler," die wir in fragment_main.xml gelegt haben, als Adapter unseren NoteAdapter und dessen Logik bekommen soll.
    
    Wir erstellen also eine variable noteAdapter, die eine Instanz unseres NoteAdapter ist, und greifen über binding auf den noteRecycler im Layout zu, und legen fest, dass dessen Adapter unser noteAdapter sein soll.

--- app/src/main/java/com/example/notes/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/notes/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.notes.NoteAdapter
 import com.example.notes.R
 import com.example.notes.databinding.FragmentMainBinding
 
@@ -20,6 +21,13 @@ class MainFragment : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val noteAdapter = NoteAdapter()
+        binding.noteRecycler.adapter = noteAdapter
     }
 
 }
