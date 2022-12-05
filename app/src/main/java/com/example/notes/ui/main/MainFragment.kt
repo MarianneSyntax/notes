06995e2cd5df28332e4b668521ef3a27e1570007
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-05 15:56:47 +0000

    Logik des Add Note Buttons: Als Index einfach -1 angeben
    FERTIG ist die Notiz App.
    Die Veränderungen werden noch nicht persistiert, also gespeichert, auch wenn wir die App zu machen. Das kommt noch ;)

--- app/src/main/java/com/example/notes/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/notes/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.notes.NoteAdapter
 import com.example.notes.R
 import com.example.notes.databinding.FragmentMainBinding
@@ -32,6 +33,10 @@ class MainFragment : Fragment() {
 
         viewModel.notes.observe(viewLifecycleOwner) {
             noteAdapter.submitList(it)
+        }
+
+        binding.addNoteBtn.setOnClickListener {
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToNoteFragment(-1))
         }
     }
 
