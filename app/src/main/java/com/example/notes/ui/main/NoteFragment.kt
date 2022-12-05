04d3671cb273a34e96f1109afe0ca200ecfb1060
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-05 15:27:27 +0000

    Jetzt wo das ViewModel mit der lösch und update Logik fertig ist, können wir ins NoteFragment zurückkehren und die onViewCreated Funktion fertigstellen, die festlegt, was die Notiz, die angezeigt werden soll, anzeigt:
    Wir sagen dem Observer, der die notes-Variable im ViewModel beobachtet: die Notiz shownNote, die wir anzeigen wollen, ist diejenige, die an der position noteIndex, zB 0, 1, etc. steht. Diesen noteIndex haben wir im nav_graph übergeben.
    
    Jetzt legen wir noch per dataBinding fest, dass die Daten der shownNote in den Titel und Body geschrieben werden.

--- app/src/main/java/com/example/notes/ui/main/NoteFragment.kt
+++ app/src/main/java/com/example/notes/ui/main/NoteFragment.kt
@@ -2,12 +2,12 @@ package com.example.notes.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Note
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import com.example.notes.data.model.Note
 import com.example.notes.databinding.FragmentNoteBinding
 import com.example.notes.ui.main.MainViewModel
 
@@ -27,7 +27,32 @@ class NoteFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val noteIndex = requireArguments().get("noteIndex")
+        val noteIndex = requireArguments().getInt("noteIndex")
+
+        if (noteIndex >= 0){
+            viewModel.notes.observe(viewLifecycleOwner){
+                shownNote = it[noteIndex]
+
+                binding.noteTitleEdit.setText(shownNote.title)
+                binding.noteBodyEdit.setText(shownNote.body)
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
