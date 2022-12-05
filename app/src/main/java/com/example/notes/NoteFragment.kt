c09be29e88b4f0b3a37b655e6e1b6cbc18eea247
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2022-12-05 14:14:23 +0000

    Jetzt die Logik des NotizFragments.
    Unnötiges rausnehmen, 3 gebrauchte Variablen dazu:
    
    ViewModel: das die Methoden hält, die die Logik betreffen und die wir beim Benutzen brauchen
    
    binding: anknüpfung ans Layout, das dem User angezeigt wird, sodass wir bei Aktionen im Layout (Klicken, was Eintippen) reagieren können
    
    shownNote: die aktuelle Notiz, die gerade angezeigt wird

--- app/src/main/java/com/example/notes/NoteFragment.kt
+++ app/src/main/java/com/example/notes/NoteFragment.kt
@@ -2,30 +2,30 @@ package com.example.notes
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.notes.databinding.FragmentNoteBinding
+import com.example.notes.ui.main.MainViewModel
 
 class NoteFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = NoteFragment()
-    }
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentNoteBinding
 
-    private lateinit var viewModel: NoteViewModel
+    private lateinit var shownNote: Note
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_note, container, false)
+        binding = FragmentNoteBinding.inflate(inflater)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
